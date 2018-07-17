2a9d4537391e588eb9e8971a806b055d15862c08
Romanov Yury <workgeos@mail.ru>
2018-07-17 15:01:16 +0700

    Add colors list

--- src/Game/buildBox.js
+++ src/Game/buildBox.js
@@ -1,15 +1,7 @@
 import * as THREE from 'three';
+import colorsList from './colors';
 
 const buildBox = (width, height, depth) => {
-  const colors = [
-    0xf25346,
-    0xd8d0d1,
-    0x59332e,
-    0xF5986E,
-    0x23190f,
-    0x68c3c0
-  ];
-
   const geometry = new THREE.BoxGeometry(width, height, depth, 1);
   /* const material = new THREE.MeshLambertMaterial({
     color: 0x003E7238,
@@ -21,7 +13,7 @@ const buildBox = (width, height, depth) => {
   }); */
 
   /* eslint-disable-next-line */
-  const material = new THREE.MeshPhongMaterial({ color: colors[Math.floor(Math.random() * colors.length)], flatShading: THREE.FlatShading });
+  const material = new THREE.MeshPhongMaterial({ color: colorsList[Math.floor(Math.random() * colorsList.length)], flatShading: THREE.FlatShading });
 
   return new THREE.Mesh(geometry, material);
 };
