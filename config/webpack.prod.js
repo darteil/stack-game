31be7c19c01e78c06a93ac864dde4638049186e4
Romanov Yurii <darteil.base@gmail.com>
2019-07-01 11:32:30 +0700

    Configure assets

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -9,29 +10,32 @@ const base = require('./webpack.base');
 const distPath = path.resolve(__dirname, '../dist');
 const publicPathFolder = path.resolve(__dirname, '../public/');
 
-module.exports = (env, argv) => merge(base(env, argv), {
-  devtool: 'source-map',
-  stats: 'errors-only',
-  output: {
-    path: distPath,
-    filename: 'js/[name].[hash:8].js'
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[hash].css'
-    }),
-    new ProgressBarPlugin(),
-    new CleanWebpackPlugin(
-      [distPath],
-      {
-        root: path.resolve(__dirname, '../'),
-      }
-    ),
-    new CopyWebpackPlugin([
-      {
-        from: publicPathFolder,
-        ignore: [ 'vendor/**/*' ]
-      }
-    ])
-  ]
-});
+module.exports = (env, argv) => {
+  let publicPath = '/stack';
+
+  return merge(base(env, argv), {
+    devtool: 'source-map',
+    stats: 'errors-only',
+    output: {
+      path: distPath,
+      filename: 'js/[name].[hash:8].js',
+      publicPath: publicPath
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[hash].css'
+      }),
+      new ProgressBarPlugin(),
+      new CleanWebpackPlugin([distPath], {
+        root: path.resolve(__dirname, '../')
+      }),
+      new HtmlWebpackTagsPlugin({ tags: ['fonts/roboto.css'], append: true }),
+      new CopyWebpackPlugin([
+        {
+          from: publicPathFolder,
+          ignore: ['vendor/**/*']
+        }
+      ])
+    ]
+  });
+};
