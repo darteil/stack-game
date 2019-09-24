7b8c49c7429c2fbb107b035e94259ca8ff610754
Romanov Yurii <darteil.base@gmail.com>
2019-09-24 16:10:00 +0700

    fix routing

--- src/index.jsx
+++ src/index.jsx
@@ -6,14 +6,14 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import GithubCorner from 'react-github-corner';
 import App from './MainPage/App';
-import { persistor, store, history } from './store';
+import { persistor, store, browserHistory } from './store';
 import './reset.css';
 import './global.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Router history={history}>
+      <Router history={browserHistory}>
         <Fragment>
           <App />
           <GithubCorner href="https://github.com/darteil/StackGame" />
