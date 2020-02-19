8325f8c0497808607df448d163f3a4ebde3ca4c3
Romanov Yuri <darteil.base@gmail.com>
2020-02-19 14:04:26 +0700

    Update developer tools
    - add ligt settings to dat.gui
    - create game settings file

---
+++ src/Game/settings.ts
@@ -0,0 +1,22 @@
+const GAME_SETTINGS = {
+  speed: 3,
+  light: {
+    directionalLightPosition: {
+      x: 100,
+      y: 1000,
+      z: 150
+    },
+    directionalLightTargetPosition: {
+      x: 36,
+      y: 0,
+      z: 36
+    },
+    hemisphereLightPosition: {
+      x: 0,
+      y: 1000,
+      z: 0
+    }
+  }
+};
+
+export default GAME_SETTINGS;
